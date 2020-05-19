61e1727c53cbe71a6ef761685d8146c2b006dfb6
Mumbere7 <arnoldnzibuk@gmmail.com>
2020-05-19 14:00:04 +0300

    formatting datetime

---
+++ lib/models/note_for_listing.dart
@@ -0,0 +1,13 @@
+class NoteForListing {
+  String noteID;
+  String noteTitle;
+  DateTime createdDateTime;
+  DateTime lastEditDateTime;
+
+  NoteForListing({
+    this.noteID,
+    this.noteTitle,
+    this.createdDateTime,
+    this.lastEditDateTime
+  });
+}
--- lib/views/note_list.dart
+++ lib/views/note_list.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:notetaker/models/note_for_listing.dart';
 
 class NoteList extends StatelessWidget {
+
+  final notes = [
+    new NoteForListing(
+        noteID: '1',
+        createdDateTime: DateTime.now(),
+        lastEditDateTime: DateTime.now(),
+        noteTitle: 'Note 1'
+    ),
+    new NoteForListing(
+        noteID: '2',
+        createdDateTime: DateTime.now(),
+        lastEditDateTime: DateTime.now(),
+        noteTitle: 'Note 2'
+    ),
+    new NoteForListing(
+        noteID: '3',
+        createdDateTime: DateTime.now(),
+        lastEditDateTime: DateTime.now(),
+        noteTitle: 'Note 3'
+    ),
+  ];
+String formatDateTime(DateTime dateTime){
+  return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
+}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,19 +37,22 @@ class NoteList extends StatelessWidget {
         child: Icon(Icons.add),
       ),
       body: ListView.separated(
-        separatorBuilder: (_, __) => Divider(
-          height: 1,
-          color: Colors.grey,
-        ),
+        separatorBuilder: (_, __) =>
+            Divider(
+              height: 1,
+              color: Colors.grey,
+            ),
         itemBuilder: (_, index) {
           return ListTile(
             title: Text(
-              'Hello',
-              style: TextStyle(color: Theme.of(context).primaryColor),
+              notes[index].noteTitle,
+              style: TextStyle(color: Theme
+                  .of(context)
+                  .primaryColor),
             ),
-            subtitle: Text('Last edited on 13/04/2020'),
+            subtitle: Text('Last edited on ${formatDateTime(notes[index].lastEditDateTime)}'),
           );
-        }, itemCount: 15,
+        }, itemCount: notes.length,
       ),
     );
   }
