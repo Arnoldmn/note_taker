9a6b992209d296381a7b8f4e8a0fa44cf5e31c32
Mumbere7 <arnoldnzibuk@gmmail.com>
2020-05-26 12:48:00 +0300

    Add insert note class

--- lib/services/note_service.dart
+++ lib/services/note_service.dart
@@ -4,6 +4,7 @@ import 'package:notetaker/models/api_response.dart';
 import 'package:notetaker/models/note.dart';
 import 'package:notetaker/models/note_for_listing.dart';
 import 'package:http/http.dart' as http;
+import 'package:notetaker/models/note_inssert.dart';
 
 class NoteService {
   static const API = 'http://api.notes.programmingaddict.com';
@@ -40,6 +41,18 @@ class NoteService {
       }
       return APIResponse<Note>(error: true, errorMessage: 'An error occurred');
     }).catchError((_) => APIResponse<List<NoteForListing>>(
+        error: true, errorMessage: 'An error occurred'));
+  }
+
+  Future<APIResponse<bool>> createNote(NoteInsert item) {
+    return http.post(API + '/notes/', headers: headers, body: item.toJson()).then((data) {
+      if (data.statusCode == 201) {
+        return APIResponse<bool>(
+          data: true,
+        );
+      }
+      return APIResponse<bool>(error: true, errorMessage: 'An error occurred');
+    }).catchError((_) => APIResponse<bool>(
         error: true, errorMessage: 'An error occurred'));
   }
 }
